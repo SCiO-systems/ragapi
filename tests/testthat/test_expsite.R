b2fe1a4cffdb49538b3b03cd71bf372ec833dbba
omarbenites <omar_cs_7@hotmail.com>
2020-10-08 16:02:36 -0500

    update r-calls version 0233

--- tests/testthat/test_expsite.R
+++ tests/testthat/test_expsite.R
@@ -2,14 +2,26 @@ context("test project agronomic experiment-site AgExpSite class and methods")
 
 ##studyDbId
 
+
 test_that("Test get exp-site description ver. 0135 with 3 sites by studyId", {
   
   test_v0135 <- AgExpSite$new(serverURL = "https://research.cip.cgiar.org/agrofims/api/dev",
                               version ="/0135/r")
-  out_test <- test_v0135$ag_get_cropsite_studyId(studyDbId= 23, format="data.frame")
-  testthat::expect_equal(object = length(out_test),expected = 3)
+  out_test2 <- test_v0135$ag_get_cropsite_studyId(studyDbId= 23, format="data.frame")
+  testthat::expect_equal(object = length(out_test2),expected = 3)
   
 })
+
+
+test_that("Test agronomic exp-sites version 0233 with 1 sites", {
+  
+  test_v0233 <- AgExpSite$new(serverURL = "https://research.cip.cgiar.org/agrofims/api/dev",
+                              version ="/0233/r")#, endPoint= "/exp-site/getAll?id=")
+  out_test <- test_v0135$ag_get_expsite_studyId(studyDbId= 2, format="data.frame")
+  testthat::expect_equal(object = nrow(out_test),expected = 3)
+  
+})
+
 
 test_that("Test agronomic exp-sites version 0135 with 3 sites", {
   
